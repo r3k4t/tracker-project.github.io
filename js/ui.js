7a8b79d0639a6a19ace0dd10364441d27aa025ab
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2021-08-03 12:52:57 +0600

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -7,19 +7,15 @@ class UI{
         this.profile.innerHTML =`<div class="card card-body mb-3">
         <div class="row">
             <div class="col-md-20">
-                <ul class="list-group">
-                    <li class="list-group-item"><b>IP :</b> ${user.query}</li>
-                    <li class="list-group-item"><b>Country :</b> ${user.country}</li>
-                    <li class="list-group-item"><b>Country Code :</b> ${user.countryCode}</li>
+                 <ul class="list-group">
+                    <li class="list-group-item"><b>IP :</b> ${user.ip}</li>
+                    <li class="list-group-item"><b>City :</b> ${user.city}</li>
                     <li class="list-group-item"><b>Region :</b> ${user.region}</li>
-                    <li class="list-group-item"><b>Region Name :</b> ${user.regionName}</li>
-                    <li class="list-group-item"><b>ISP :</b>${user.isp}</li>
-                    <li class="list-group-item"><b>Lat :</b>${user.lat}</li>
-                    <li class="list-group-item"><b>Lon :</b>${user.lon}</li>
+                    <li class="list-group-item"><b>Country :</b> ${user.country}</li>
+                    <li class="list-group-item"><b>Loc :</b> ${user.loc}</li>
                     <li class="list-group-item"><b>Org :</b>${user.org}</li>
-                    <li class="list-group-item"><b>Zip :</b>${user.zip}</li>
                     <li class="list-group-item"><b>Timezone :</b>${user.timezone}</li>
-                    <li class="list-group-item"><b>Google Map Link :</b><a href="https://google.com/maps/place/${user.lat},${user.lon}">https://google.com/maps/place/${user.lat},${user.lon}</a>            
+                    <li class="list-group-item"><b>Google Map Link :</b><a href="https://google.com/maps/place/${user.loc}">https://google.com/maps/place/${user.loc}</a>            
                 </ul>
             </div>
         </div>
